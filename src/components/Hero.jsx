20f6b964674ea83956a7b7dc9488f5652ca68a15
Vitalii <co4ckowoi.v@gmail.com>
2025-07-03 10:04:38 +0300

    add Hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { SplitText } from 'gsap/all'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+  const videoRef = useRef()
+
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+
   useGSAP(() => {
     const heroSplit = new SplitText('.title', {
       type: 'chars, words',
@@ -29,57 +35,95 @@ const Hero = () => {
       stagger: 0.06,
       delay: 1,
     })
-  })
 
-  gsap
-    .timeline({
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: '#hero',
+          start: 'top top',
+          end: 'bottom top',
+          scrub: true,
+        },
+      })
+      .to('.right-leaf', { y: 200 }, 0)
+      .to('.left-leaf', { y: -200 }, 0)
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%'
+    const endValue = isMobile ? '120% top' : 'bottom top'
+
+    let tl = gsap.timeline({
       scrollTrigger: {
-        trigger: '#hero',
-        start: 'top top',
-        end: 'bottom top',
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
         scrub: true,
+        pin: true,
       },
     })
-    .to('.right-leaf', { y: 200 }, 0)
-    .to('.left-leaf', { y: -200 }, 0)
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      })
+    }
+  }, [])
+
+  /*     videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+       currentTime: videoRef.current.duration,
+      });
+     };
+    }, []); */
 
   return (
-    <section id='hero' className='noisy'>
-      <h1 className='title'>MOJITO</h1>
+    <>
+      <section id='hero' className='noisy'>
+        <h1 className='title'>MOJITO</h1>
 
-      <img
-        src='/images/hero-left-leaf.png'
-        alt='left-leaf'
-        className='left-leaf'
-      />
-      <img
-        src='/images/hero-right-leaf.png'
-        alt='right-leaf'
-        className='right-leaf'
-      />
+        <img
+          src='/images/hero-left-leaf.png'
+          alt='left-leaf'
+          className='left-leaf'
+        />
+        <img
+          src='/images/hero-right-leaf.png'
+          alt='right-leaf'
+          className='right-leaf'
+        />
 
-      <div className='body'>
-        {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+        <div className='body'>
+          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
 
-        <div className='content'>
-          <div className='space-y-5 hidden md:block'>
-            <p>Cool. Crisp. Classic.</p>
-            <p className='subtitle'>
-              Sip the Spirit <br /> of Summer
-            </p>
-          </div>
+          <div className='content'>
+            <div className='space-y-5 hidden md:block'>
+              <p>Cool. Crisp. Classic.</p>
+              <p className='subtitle'>
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
 
-          <div className='view-cocktails'>
-            <p className='subtitle'>
-              Every cocktail on our menu is a blend of premium ingredients,
-              creative flair, and timeless recipes — designed to delight your
-              senses.
-            </p>
-            <a href='#cocktails'>View cocktails</a>
+            <div className='view-cocktails'>
+              <p className='subtitle'>
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+              <a href='#cocktails'>View cocktails</a>
+            </div>
           </div>
         </div>
+      </section>
+
+      <div className='video absolute inset-0'>
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          preload='auto'
+          src='/videos/output.mp4'
+        />
       </div>
-    </section>
+    </>
   )
 }
 
